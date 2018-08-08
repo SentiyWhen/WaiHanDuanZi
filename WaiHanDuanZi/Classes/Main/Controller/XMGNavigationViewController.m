5e716be891677ad9b8e2a5fa9c06fc087e057003
张文文 <18622076193@163.com>
2018-08-08 10:45:45 +0800

    滑动返回

--- WaiHanDuanZi/Classes/Main/Controller/XMGNavigationViewController.m
+++ WaiHanDuanZi/Classes/Main/Controller/XMGNavigationViewController.m
@@ -8,7 +8,7 @@
 
 #import "XMGNavigationViewController.h"
 
-@interface XMGNavigationViewController ()
+@interface XMGNavigationViewController ()<UIGestureRecognizerDelegate>
 
 @end
 
@@ -27,12 +27,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    // 控制手势什么时候触发,只有非根控制器才需要触发手势
+    self.interactivePopGestureRecognizer.delegate = self;
+}
+
+#pragma mark - UIGestureRecognizerDelegate
+// 决定是否触发手势
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *)touch{
+    return self.childViewControllers.count > 1;
 }
 
 - (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated {
     // 设置返回按钮,只有非根控制器
     if (self.childViewControllers.count > 0) {
+        // 恢复滑动返回功能 -> 分析:把系统的返回按钮覆盖 -> 1.手势失效(1.手势被清空 2.可能手势代理做了一些事情,导致手势失效)
+        
         viewController.navigationItem.leftBarButtonItem = [UIBarButtonItem backItemWithimage:[UIImage imageNamed:@"navigationButtonReturn"] highImage:[UIImage imageNamed:@"navigationButtonReturnClick"]  target:self action:@selector(back) title:@"返回"];
     }
     // 真正在跳转
