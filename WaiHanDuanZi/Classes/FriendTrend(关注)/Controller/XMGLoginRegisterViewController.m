38f652f04bcfa183b4aa57695bd65dc1fcbbb86d
张文文 <18622076193@163.com>
2018-08-10 16:42:28 +0800

    输入视图布局处理

--- WaiHanDuanZi/Classes/FriendTrend(关注)/Controller/XMGLoginRegisterViewController.m
+++ WaiHanDuanZi/Classes/FriendTrend(关注)/Controller/XMGLoginRegisterViewController.m
@@ -11,6 +11,7 @@
 
 @interface XMGLoginRegisterViewController ()
 @property (weak, nonatomic) IBOutlet UIView *middleView;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *leadCons;
 
 @end
 
@@ -20,13 +21,35 @@
 }
 - (IBAction)clickRegister:(UIButton *)sender {
     sender.selected = !sender.selected;
+    //平移中间view
+    _leadCons.constant = _leadCons.constant == 0 ? -self.middleView.xmg_width * 0.5 : 0;
+    [UIView animateWithDuration:0.3 animations:^{
+        [self.view layoutIfNeeded];
+    }];
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    /*
+     屏幕适配:
+     1.一个view从xib加载,需不需在重新固定尺寸 一定要在重新设置一下
+     
+     2.在viewDidLoad设置控件frame好不好,开发中一般在viewDidLayoutSubviews布局子控件
+     
+     */
+    
+    // 创建登录view
+    XMGLoginRegisterView *loginView = [XMGLoginRegisterView loginView];
+    
+    // 添加到中间的view
+    [self.middleView addSubview:loginView];
+    
+    // 添加注册界面
     XMGLoginRegisterView *registerView = [XMGLoginRegisterView registerView];
+    
+    // 添加到中间的view
     [self.middleView addSubview:registerView];
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -34,14 +57,16 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)viewDidLayoutSubviews{
+    [super viewDidLayoutSubviews];
+    
+    XMGLoginRegisterView *loginView = self.middleView.subviews[0];
+    loginView.frame = CGRectMake(0, 0, self.middleView.xmg_width * 0.5, self.middleView.xmg_height);
+    
+    XMGLoginRegisterView *registerView = self.middleView.subviews[1];
+    registerView.frame = CGRectMake( self.middleView.xmg_width * 0.5, 0,self.middleView.xmg_width * 0.5, self.middleView.xmg_height);
+    
 }
-*/
+
 
 @end
