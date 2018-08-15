7b323014b8ddb4dbc71542f1af4ae926f144c586
张文文 <18622076193@163.com>
2018-08-15 10:13:55 +0800

    添加标题栏按钮

--- Whdz/Classes/Essence(精华)/Controller/XMGEssenceViewController.m
+++ Whdz/Classes/Essence(精华)/Controller/XMGEssenceViewController.m
@@ -14,10 +14,12 @@
 
 @interface XMGEssenceViewController ()
 
+@property (nonatomic, weak) UIView *titlesView;
+
 @end
 
 @implementation XMGEssenceViewController
-
+#pragma mark - 初始化
 - (void)viewDidLoad {
     [super viewDidLoad];
     //导航栏
@@ -40,6 +42,28 @@
     titlesView.backgroundColor = [[UIColor whiteColor] colorWithAlphaComponent:0.5];
     titlesView.frame = CGRectMake(0, 64, self.view.xmg_width, 35);
     [self.view addSubview:titlesView];
+    self.titlesView = titlesView;
+    
+    //标题栏按钮
+    [self setupTitlesButtons];
+}
+
+- (void)setupTitlesButtons {
+    NSArray *titles = @[@"全部",@"视频",@"声音",@"图片",@"段子"];
+    NSUInteger count = titles.count;
+    //标题按钮尺寸
+    CGFloat titleButtonW = self.titlesView.xmg_width / count;
+    CGFloat titleButtonH = self.titlesView.xmg_height;
+    //创建5个标题按钮
+    for (NSUInteger i = 0; i < count; i++) {
+        UIButton *titleButton  = [[UIButton alloc] init];
+        [titleButton addTarget:self action:@selector(titlesButtonClick:) forControlEvents:UIControlEventTouchUpInside];
+        [self.titlesView addSubview:titleButton];
+        titleButton.frame = CGRectMake(titleButtonW*i, 0, titleButtonW, titleButtonH);
+        [titleButton setTitle:titles[i] forState:UIControlStateNormal];
+        [titleButton setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
+        [titleButton setTitleColor:[UIColor redColor] forState:UIControlStateSelected];
+    }
 }
 
 - (void)setupNavBar {
@@ -51,7 +75,12 @@
     self.navigationItem.titleView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"MainTitle"]];
 }
 
+#pragma mark - 监听
 - (void)game {
+    XMGFunc;
+}
+
+- (void)titlesButtonClick :(UIButton *)titleButton {
     XMGFunc;
 }
 
